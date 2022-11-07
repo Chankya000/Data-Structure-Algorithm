4e6d435d315fa6ff49733e63b1908768c49eb8ca
Chanakya000 <sgrsharma720@gmail.com>
2022-11-07 11:25:15 -0500

    second pattern

---
+++ Pattern/src/Pattern2.java
@@ -0,0 +1,31 @@
+// Question statement
+//create a pattern for variable n like below
+//n =5
+//*   *   *   *   *
+//*   *   *   *
+//*   *   *
+//*   *
+//*
+import java.util.*;
+
+
+public class Pattern2 {
+
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int nstars = n;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nstars; j++) {
+                System.out.print("*\t");
+            }
+            nstars--;
+            System.out.println();
+        }
+
+    }
+
+
+}
