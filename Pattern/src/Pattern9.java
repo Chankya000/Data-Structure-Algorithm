d26cae1fbf6966f4cecb33ddca797a900c7c3337
Chanakya000 <sgrsharma720@gmail.com>
2022-11-10 10:55:04 -0500

    2qsn

--- Pattern/src/Pattern9.java
+++ Pattern/src/Pattern9.java
@@ -8,7 +8,7 @@
 //     *                     *
 
 
-// to be worked on
+
 import java.util.Scanner;
 
 public class Pattern9 {
@@ -16,33 +16,15 @@ public class Pattern9 {
     public static void main(String[] args) {
         System.out.println("Please enter the value of n");
         Scanner scn = new Scanner(System.in);
-        int n= scn.nextInt();
-        int isp=(n/2)+1;
-        int osp = 0;
-        for(int i=1;i<=n;i++){
-            //display outer space
-            for(int o =0;o<osp;o++){
-                System.out.print("\t");
-            }
-            //display first star
-            System.out.print("*\t");
-
-            //display inner space
-            for(int j=1;j<=isp;j++){
-                System.out.print("\t");
-            }
-
-            //display second star conditionally
-            if(i!=(n/2)+1){
-                System.out.print("*\t");
-            }
-                if(i<=n/2){
-                    isp=isp-2;
-                    ++osp;
+        int n = scn.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for(int j=1;j<=n;j++){
+                if(i==j||i+j==n+1){
+                    System.out.print("*\t");
                 }else{
-                    isp=isp+2;
-                    --osp;
+                    System.out.print("\t");
                 }
+            }
             System.out.println();
         }
     }
